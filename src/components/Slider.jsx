a46d0bae8d12a8098f690b2c13e8f97684c1c21c
babinicz <babinicz@onet.pl>
2021-09-06 10:08:23 +0200

    dodałem alt

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -127,7 +127,7 @@ const Slider = () => {
         {sliderItems.map((item) => (
           <Slide bg={item.bg} key={item.id}>
             <ImgContainer>
-              <Image src={item.img} />
+              <Image src={item.img} alt="..." />
             </ImgContainer>
             <InfoContainer>
               <Title>{item.title}</Title>
